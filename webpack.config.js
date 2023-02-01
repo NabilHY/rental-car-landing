449afe578798decfd0da9574a334bff7bfd9916f
Nabil <hayounn.etd@gmail.com>
2023-02-01 16:14:14 +0100

    Create the navbar compoenent on the entry point

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: [
+    './src/index.js',
+    'jquery',
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
